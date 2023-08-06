9623bdd392d536e6a52134510b9a74f299282276
ashu <k.ashu403@gmail.com>
2023-08-06 16:43:13 -0400

    Final code changes and debug

--- src/activity.py
+++ src/activity.py
@@ -2,15 +2,16 @@ from src.database import Database
 
 
 class Activity:
-    def __init__(self, name, description, address, price):
+    def __init__(self, name, description, address, price,skill):
         self.name = name
         self.description = description
         self.address = address
         self.price = price
+        self.skill = skill
         
     def save(self):
-        query = "INSERT IGNORE INTO job_posting (company_name ,description,address, salary) VALUES (%s, %s, %s, %s)"
-        values = (self.name,self.description,self.address,self.price)
+        query = "INSERT IGNORE INTO job_posting (company_name ,description,address, salary,skills) VALUES (%s, %s, %s, %s,%s)"
+        values = (self.name,self.description,self.address,self.price,self.skill)
         print(query, values)
         db = Database().get_db()
         cursor = db.cursor()
